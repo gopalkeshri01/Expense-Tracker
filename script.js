7d8657aad657910438c30ba512c20bfa879370a4
Gopal Kumar Keshri <gopalkumarkeshri549@gmail.com>
2025-02-21 20:58:01 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const expenseForm = document.getElementById('expenseForm');
+// console.log(expenseForm);
+const expenseList = document.getElementById('expenseList');
+// console.log(expenseList)
+expenseForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const description = document.getElementById('description').value;
+    const category = document.getElementById('category').value;
+    const amount = document.getElementById('amount').value;
+
+    if (description && category && !isNaN(amount)) {
+        const newRow = document.createElement('tr')
+        newRow.innerHTML = `<td>${description}</td> <td>${category}</td> <td>${amount}</td>`;
+        expenseList.appendChild(newRow);
+        document.getElementById('description').value = "";
+        document.getElementById('category').value = "";
+        document.getElementById('amount').value = "";
+    }
+})
